d6e39fd715ee2c0576159ce6adf93ff27c680a7b
Pamela <contatopamela@outlook.com>
2024-09-04 16:24:45 -0300

    Adicionadas rotas e atualizado componente Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import './App.css';
 import Home from './pages/home/Home';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
-import ListarCategorias from './components/categorias/listacategoria/ListaCategoria';
-import FormCategoria from './components/categorias/formcategoria/FormCategoria';
-import DeletarCategoria from './components/categorias/deletarcategoria/DeletarCategoria';
+import FormCategoria from './components/categoria/formcategoria/FormCategoria';
+import ListarCategorias from './components/categoria/listacategoria/ListaCategoria';
+import DeletarCategoria from './components/categoria/deletarcategoria/DeletarCategoria';
+
 
 function App() {
   return (
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,19 +7,32 @@ function Footer() {
     <footer className='flex flex-col items-center bg-pink-500 text-white py-6'>
       <p className='text-lg font-bold'>Farmácia Saúde & Vida | Copyright: {year}</p>
       <div className='flex gap-4 mt-2'>
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.facebook.com/itpaam/"  
+          rel="noopener noreferrer"
+          aria-label="Facebook"
+        >
           <FacebookLogo size={24} />
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.instagram.com/itpaam/"  
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
           <InstagramLogo size={24} />
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.linkedin.com/in/pamelasalgadoa/" 
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
           <LinkedinLogo size={24} />
         </a>
       </div>
     </footer>
   );
 }
-
 
 export default Footer;
